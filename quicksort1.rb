217b167ae886bd6e9c1791be896e37aa990a24fb
Aniket Pant <me@aniketpant.com>
2013-06-27 20:04:34 +0530

    Add quicksort1 problem
    Signed-off-by: Aniket Pant <me@aniketpant.com>

---
+++ quicksort1.rb
@@ -0,0 +1,16 @@
+# quicksort1
+# https://www.hackerrank.com/challenges/quicksort1
+
+def  partition( ar)
+  pivot = [ar.first.to_i]
+  ar = ar.map {|x| x.to_i}
+  lessThanPivot = ar.select{|x| x < pivot.first}
+  moreThanPivot = ar.select{|x| x > pivot.first}
+  list = lessThanPivot + pivot + moreThanPivot
+  list.map{|x| print "#{x} "}
+  puts "\n"
+end
+# Tail starts here
+cnt = gets.to_i;
+ar = STDIN.gets.chomp.split(" ");
+partition(ar);
