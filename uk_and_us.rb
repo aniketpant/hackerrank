e2f4b5d252c6b394c79c8308519947a9f0a7d984
Aniket Pant <me@aniketpant.com>
2013-06-28 11:11:37 +0530

    Add second regex code
    Signed-off-by: Aniket Pant <me@aniketpant.com>

---
+++ uk_and_us.rb
@@ -0,0 +1,28 @@
+# uk-and-us
+# https://www.hackerrank.com/challenges/uk-and-us
+
+def uk_and_us( lines, tests)
+  tests.each do |test|
+    count = 0
+    us_spelling = test
+    uk_spelling = /\w+(?=ze)/.match(test).to_s + "se"
+    lines.each do |line|
+      match = line.scan(/(#{us_spelling}|#{uk_spelling})/)
+      count += match.size if match != nil
+    end
+    puts count
+  end
+end
+N = gets.to_i
+lines = []
+N.times do
+  lines << STDIN.readline.strip
+end
+
+T = gets.to_i
+tests = []
+T.times do
+  tests << STDIN.readline.strip
+end
+
+uk_and_us( lines, tests )
